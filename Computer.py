e8417ec2b9ef3ca2720700f1f39b9abeb94e2c63
yuda <yudhaprama@icloud.com>
2019-04-03 11:20:11 +0700

    init

---
+++ Computer.py
@@ -0,0 +1,68 @@
+# Class that runs all games and stores them into a file/list
+from random import randint
+import itertools
+import operator
+
+
+class Computer(object):
+	
+	# Contains a win list to store all games won, similarly a loss list
+	def __init__(self, board):
+		self.winList = []
+		self.lossList = []
+		self.board = board
+	
+	# Play games a specific number of times
+	def playGames(self, numberOfGames):
+		
+		for game in range(numberOfGames):
+			self.playSingleGame()
+	
+	# Plays a single game with the board provided and resets values after play
+	def playSingleGame(self):
+		
+		while self.board.isWin() is 0:
+			self.board.makeMove(randint(0, 8))
+		
+		# Triggers if someone wins. "Win" here is player 1 winning and "Loss" implies a player 2 win.
+		if self.board.isWin() is 1:
+			if self.board.playerToMove is 2:
+				self.winList.append(self.board.moveList[:])
+			elif self.board.playerToMove is 1:
+				self.lossList.append(self.board.moveList[:])
+		
+		self.board.resetBoard()
+	
+	# Makes a move against human player after getting a move that won the most in the same position
+	def makeMove(self, moveNumber, board):
+		
+		# List that contains all moves played at the position on the board
+		commonList = []
+		
+		for game in self.winList:
+			if game[:len(board.moveList)] == board.moveList:
+				commonList.append(game[moveNumber])
+		
+		# Makes the move most played and won the most games in the randomised games against itself
+		board.makeMove(self.most_common(commonList))
+	
+	# Gives most common element out of a list
+	def most_common(self, L):
+		# get an iterable of (item, iterable) pairs
+		SL = sorted((x, i) for i, x in enumerate(L))
+		# print 'SL:', SL
+		groups = itertools.groupby(SL, key=operator.itemgetter(0))
+		
+		# auxiliary function to get "quality" for an item
+		def _auxfun(g):
+			item, iterable = g
+			count = 0
+			min_index = len(L)
+			for _, where in iterable:
+				count += 1
+				min_index = min(min_index, where)
+			# print 'item %r, count %r, minind %r' % (item, count, min_index)
+			return count, -min_index
+		
+		# pick the highest-count/earliest item
+		return max(groups, key=_auxfun)[0]
---
+++ main.py
@@ -0,0 +1,31 @@
+from Board import Board
+from Computer import Computer
+
+if __name__ == '__main__':
+	mainBoard = Board()
+	mainComputer = Computer(mainBoard)
+	
+	# Computer saves data for n random games
+	mainComputer.playGames(1000000)
+	
+	# Start Human-Computer game here
+	
+	while (mainBoard.isWin() is not 1) and (mainBoard.movesPlayed is not 9) and (mainBoard.isWin() is not 2):
+		
+		print("Move " + str(mainBoard.movesPlayed))
+		
+		mainComputer.makeMove(mainBoard.movesPlayed, mainBoard)
+		
+		mainBoard.displayBoard()
+		
+		if mainBoard.isWin():
+			print("Computer won!")
+			break
+		
+		move = input("Enter square to move on: ")
+		
+		mainBoard.makeMove(int(move))
+		
+		mainBoard.displayBoard()
+		
+		print("Win value : " + str(mainBoard.isWin()))
